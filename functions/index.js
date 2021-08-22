d721e2febe3a18cbdf040c779e0bb0e976051c0e
Yev <yevgeniy.bidusenko@epitech.eu>
2021-08-22 23:08:03 +0200

    build firebase

---
+++ functions/index.js
@@ -0,0 +1,17 @@
+// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
+const functions = require('firebase-functions');
+
+// The Firebase Admin SDK to access Firestore.
+const admin = require('firebase-admin');
+admin.initializeApp();
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+
+const universal = require(`${process.cwd()}/dist/server`).app();
+
+exports.ssr = functions.https.onRequest(universal);
